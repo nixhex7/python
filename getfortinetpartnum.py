69eacb983a0d8d58d6b0a59bc09e2ab113b5bb18
nixhex7 <brad@nixhex.com>
2014-05-07 11:51:01 -0400

    Initial Fortinet Python Script Upload
    Uploaded two scripts:
    fortinetflashcheck.py - checks to see if router has flash issue
    getfortinetpartnum.py.  - checks to see if part number is "06"

---
+++ getfortinetpartnum.py
@@ -0,0 +1,40 @@
+import socket
+import re
+import paramiko
+with open('hosts') as f:
+    for line in f:
+        line = line.strip()
+        dssh = paramiko.SSHClient()
+        dssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if len(re.findall('[A-Za-z]',line)) > 0:
+            try:
+		hostname = line
+                line = socket.gethostbyname(line)
+            except:
+                print "Error polling DNS for %s" % (line)
+        else:
+            line = line
+        try:
+            dssh.connect(line, username='username', password='password')
+        except paramiko.ssh_exception.AuthenticationException:
+            print "Authentication error recived when connecting to %s" % (line)
+            continue
+        except:
+            print "Error connecting to %s :: socket error" % (line)
+            continue
+        print 'running script on store: %s' % (line)
+        try:
+          stdin, stdout, stderr = dssh.exec_command('get system status')
+        except:
+          print "error sending command to device"
+          continue
+        mystring = stdout.read()
+        x = mystring.split('\n')
+        for i in x:
+            a = re.findall('System Part-Number: P08943-06',i)
+            if len(a) > 0:
+                log = open('output.txt', 'a+')
+                log.write(hostname+'\n')
+                log.write(i+'\n')
+                log.close()
+    dssh.close()
